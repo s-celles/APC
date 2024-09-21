b824a3cb6c1e5e1611e29da22a1ca24bfaf24c90
scls19fr <s.celles@gmail.com>
2024-09-21 11:46:12 +0200

    release

--- apc/release.py
+++ apc/release.py
@@ -1,14 +1,8 @@
 name = "apc"
 major = "0"
-minor = "1"
+minor = "2"
 patch = "0"
 
 __version__ = ".".join((major, minor, patch))  # http://semver.org/
 
-__date__ = "2017-10-04"  # YYYY-MM-DD
-
-__description__ = "APC Network Power Management Controller"
-
-__author__ = "Payton Quackenbush"
-__email__ = ""
-__contributors__ = ["Sebastien Celles"]
+__date__ = "2024-09-21"  # YYYY-MM-DD
